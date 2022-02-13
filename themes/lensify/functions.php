9d0e3b86f8c20071e9e55e36f4476982dc788016
Karan Chauhan <karanmilanbhai.chauhan@dcmail.ca>
2022-02-13 12:11:24 +0530

    Changed menu id from 1 to primary

--- themes/lensify/functions.php
+++ themes/lensify/functions.php
@@ -49,7 +49,7 @@ function lensify_setup() {
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus(
 		array(
-			'menu-1' => esc_html__( 'Primary', 'lensify' ),
+			'menu-primary' => esc_html__( 'Primary', 'lensify' ),
 		)
 	);
 

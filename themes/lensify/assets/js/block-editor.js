8381fcca3ffa3088b9681901b124c4e87e8f91f1
Karan Chauhan <karanmilanbhai.chauhan@dcmail.ca>
2022-02-14 05:47:17 +0530

    Removed unwanted code from block-editor.css and block-editor.js

--- themes/lensify/assets/js/block-editor.js
+++ themes/lensify/assets/js/block-editor.js
@@ -1,9 +1,9 @@
-wp.blocks.registerBlockStyle( 'core/quote', {
-    name: 'fancy-quote',
-    label: 'Fancy Quote',
+wp.blocks.registerBlockStyle( 'core/heading', {
+    name: 'primary-heading',
+    label: 'Primary Heading',
 } );
 
-wp.domReady( function () {
-    wp.blocks.unregisterBlockStyle( 'core/quote', 'large' );
-    wp.blocks.unregisterBlockStyle( 'core/quote', 'plain' );
-} );
+// wp.domReady( function () {
+//     wp.blocks.unregisterBlockStyle( 'core/quote', 'large' );
+//     wp.blocks.unregisterBlockStyle( 'core/quote', 'plain' );
+// } );

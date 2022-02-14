5becb6e246dee34910a7b1511a5c2b52063f9fd0
Karan Chauhan <karanmilanbhai.chauhan@dcmail.ca>
2022-02-15 01:28:42 +0530

    Updated header

--- themes/lensify/header.php
+++ themes/lensify/header.php
@@ -25,27 +25,18 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'lensify' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
+	<?php if ( is_front_page() ) :?>
+	<header id="masthead" class="site-header white-header grid-x align-middle">
+	<?php else : ?>
+	<header id="masthead" class="site-header grid-x  align-middle">
+	<?php endif; ?>
+		<div class="site-branding cell small-12 medium-3">
 			<?php
 			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$lensify_description = get_bloginfo( 'description', 'display' );
-			if ( $lensify_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $lensify_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
+			?>
 		</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
+		<nav id="site-navigation" class="main-navigation cell small-12 medium-9">
 			<?php
 			wp_nav_menu(
 				array(

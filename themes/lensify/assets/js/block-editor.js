2e71a063910474f3aef4f6c3b5b2cc4a6f3e353a
Karan Chauhan <karanmilanbhai.chauhan@dcmail.ca>
2022-02-18 22:15:19 +0530

    Final edits for block-editor and block-editor.js

--- themes/lensify/assets/js/block-editor.js
+++ themes/lensify/assets/js/block-editor.js
@@ -1,9 +1,39 @@
-wp.blocks.registerBlockStyle( 'core/heading', {
-    name: 'primary-heading',
-    label: 'Primary Heading',
+wp.domReady( function () {
+    wp.blocks.unregisterBlockStyle( 'core/button', 'outline' );
 } );
 
-// wp.domReady( function () {
-//     wp.blocks.unregisterBlockStyle( 'core/quote', 'large' );
-//     wp.blocks.unregisterBlockStyle( 'core/quote', 'plain' );
-// } );
+wp.blocks.registerBlockStyle( 'core/column', {
+    name: 'gray-border',
+    label: 'Gray Bordert',
+} );
+wp.blocks.registerBlockStyle( 'core/column', {
+    name: 'teal-border',
+    label: 'Teal Border',
+} );
+
+wp.blocks.registerBlockStyle( 'core/media-text', {
+    name: 'border-mediatext',
+    label: 'Border Text',
+} );
+
+wp.domReady( function () {
+    wp.blocks.unregisterBlockStyle( 'core/image', 'default' );
+} );
+
+wp.blocks.registerBlockStyle( 'core/group', {
+    name: 'group-styled',
+    label: 'Group Styled',
+} );
+
+wp.domReady( function () {
+    wp.blocks.unregisterBlockStyle( 'core/quote', 'plain' );
+} );
+
+wp.blocks.registerBlockStyle( 'core/table', {
+    name: 'gray-table',
+    label: 'Gray Table',
+} );
+wp.blocks.registerBlockStyle( 'core/table', {
+    name: 'teal-table',
+    label: 'Teal Table',
+} );

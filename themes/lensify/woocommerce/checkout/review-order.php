45cb62ca826eecec5f00594dd7995a48ae28b31f
Karan Chauhan <karanmilanbhai.chauhan@dcmail.ca>
2022-03-08 19:12:08 +0530

    Review order edited.

---
+++ themes/lensify/woocommerce/checkout/review-order.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Review order table
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/checkout/review-order.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates
+ * @version 5.2.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+?>
+
+<?php
+	do_action( 'woocommerce_review_order_before_cart_contents' );
+
+	foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
+		$_product = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
+
+		if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_checkout_cart_item_visible', true, $cart_item, $cart_item_key ) ) {
+			?>
+			<div class="<?php echo esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) ); ?>">
+				<div class="lensify-checkout-product-info">
+					<div class="product-name">
+						<?php echo wp_kses_post( apply_filters( 'woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key ) ) . '&nbsp;'; ?>
+					</div>
+					<div class="lensify-product-data">
+						<?php echo wc_get_formatted_cart_item_data( $cart_item ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+					</div>
+				</div>
+				<div class="lensify-product-quantity-wrapper">
+					<?php echo apply_filters( 'woocommerce_checkout_cart_item_quantity', ' <strong class="product-quantity">' . sprintf( '&times;&nbsp;%s', $cart_item['quantity'] ) . '</strong>', $cart_item, $cart_item_key ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+				</div>
+			</div>
+			<?php
+		}
+	}
+	
+	do_action( 'woocommerce_review_order_after_cart_contents' );
+	
+?>

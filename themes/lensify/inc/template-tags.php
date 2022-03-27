6b38db0ae1745a189b06936dd136810649befed6
Karan Chauhan <karanmilanbhai.chauhan@dcmail.ca>
2022-03-27 14:05:28 +0530

    Edited template-tags.php

--- themes/lensify/inc/template-tags.php
+++ themes/lensify/inc/template-tags.php
@@ -12,27 +12,56 @@ if ( ! function_exists( 'lensify_posted_on' ) ) :
 	 * Prints HTML with meta information for the current post-date/time.
 	 */
 	function lensify_posted_on() {
-		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
 		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+			$post_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
+			$update_string = '<time class="updated" datetime="%3$s">%4$s</time>';
+
+			$post_string = sprintf(
+				$post_string,
+				esc_attr( get_the_date( DATE_W3C ) ),
+				esc_html( get_the_date() ),
+				esc_attr( get_the_modified_date( DATE_W3C ) ),
+				esc_html( get_the_modified_date() )
+			);
+			$update_string = sprintf(
+				$update_string,
+				esc_attr( get_the_date( DATE_W3C ) ),
+				esc_html( get_the_date() ),
+				esc_attr( get_the_modified_date( DATE_W3C ) ),
+				esc_html( get_the_modified_date() )
+			);
+			
+			$posted_on = sprintf(
+				/* translators: %s: post date. */
+				esc_html_x( 'Posted on %s', 'post date', 'lensify' ),
+				'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $post_string . '</a>'
+			);
+			$updated_on = sprintf(
+				/* translators: %s: update date. */
+				esc_html_x( 'Updated on %s', 'update date', 'lensify' ),
+				'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $update_string . '</a>'
+			);
+
+			echo '<span class="posted-on">' . $posted_on . '</span> <span class="updated-on">' . $updated_on . '</span>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+
+		} else {
+			$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+
+			$time_string = sprintf(
+				$time_string,
+				esc_attr( get_the_date( DATE_W3C ) ),
+				esc_html( get_the_date() ),
+				esc_attr( get_the_modified_date( DATE_W3C ) ),
+				esc_html( get_the_modified_date() )
+			);
+			$posted_on = sprintf(
+				/* translators: %s: post date. */
+				esc_html_x( 'Posted on %s', 'post date', 'lensify' ),
+				'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+			);
+
+			echo '<span class="posted-on">' . $posted_on . '</span>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 		}
-
-		$time_string = sprintf(
-			$time_string,
-			esc_attr( get_the_date( DATE_W3C ) ),
-			esc_html( get_the_date() ),
-			esc_attr( get_the_modified_date( DATE_W3C ) ),
-			esc_html( get_the_modified_date() )
-		);
-
-		$posted_on = sprintf(
-			/* translators: %s: post date. */
-			esc_html_x( 'Posted on %s', 'post date', 'lensify' ),
-			'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
-		);
-
-		echo '<span class="posted-on">' . $posted_on . '</span>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-
 	}
 endif;
 

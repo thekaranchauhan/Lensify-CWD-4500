07501c7f937cffccd0c178d62c104d6f0d357bb5
Karan Chauhan <karanmilanbhai.chauhan@dcmail.ca>
2022-03-28 11:51:14 +0530

    Updated cart-replacer.js

--- themes/lensify/assets/js/cart-replacer.js
+++ themes/lensify/assets/js/cart-replacer.js
@@ -1,14 +1,18 @@
-let variations = document.querySelectorAll('dt');
+window.onload = function() {
+    let variations = document.querySelectorAll('dt');
 
-for (let variation of variations) {
-    let text = variation.innerText;
-    let newText;
-    if (text=="Choose your lens") {
-        newText = "Colour:";
-    } else if (text=="Choose a color") {
-        newText = "Design Colour:";
-    } else {
-        newText = text.replace("::", ":");
+    for (let variation of variations) {
+        let text = variation.innerText;
+        let newText;
+        if (text=="Choose a frame for your lens::") {
+            newText = "Frame:";
+        } else if (text=="Choose a colour for your lens::") {
+            newText = "Color:";
+        } else if (text=="Choose a lens:") {
+            newText = "Lens:";
+        } else {
+            newText = text.replace("::", ":");
+        }
+        variation.innerText = newText;
     }
-    variation.innerText = newText;
 }
---
+++ themes/lensify/assets/js/product-text-replacer.js
@@ -0,0 +1,14 @@
+let uploadHeader = document.querySelector('.file_title_clean');
+console.log(uploadHeader);
+
+let text = uploadHeader.innerText;
+let newText = text.replace('files', 'my lens');
+uploadHeader.innerText = newText;
+
+let selectedVariation = document.querySelectorAll('.woo-selected-variation-items-name');
+selectedVariation.forEach(variation => {
+    console.log(variation);
+    let selectedVariationTxt = variation.innerText;
+    let newVariationTxt = selectedVariationTxt.replace(': ', '');
+    variation.innerText = newVariationTxt;
+}); 
